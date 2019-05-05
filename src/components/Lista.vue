dce5dfa4c3c1fd044d4d369850dd77d66cc512b4
egsded <sarcasmo1601@gmail.com>
2019-05-04 22:45:40 -0500

    pruebon

---
+++ src/components/Lista.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<b-list-group>
+  <b-list-group-item v-for="(item, index) of mugrero" :key="item.id" @click="aumentar(index)"
+   class="d-flex justify-content-between align-items-center">
+    {{index}} - {{item.nombre}}
+    <b-badge variant="primary" pill>{{item.cantidad}}</b-badge>
+  </b-list-group-item>
+</b-list-group>
+<b-button block variant="danger" @click="reiniciar">Reiniciar</b-button>
+	</div>
+</template>
+<script>
+import {mapState, mapMutations} from 'vuex';
+	export default{
+		name: 'Lista',
+		computed:{
+			...mapState(['frutas']),
+			mugrero(){
+				return this.frutas.sort( (a, b) => b.cantidad - a.cantidad);
+			}
+		},
+		methods:{
+			...mapMutations(['aumentar', 'reiniciar'])
+		}
+	}
+</script>
